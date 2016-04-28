81b78772d1099b15e6f20875bc39b0ab3a56e738
Sergii <sergii.dobzhynetskyi@yahoo.com>
2016-04-28 17:27:29 +0300

    milliseconds?

---
+++ js_hw5-6/js/timerScript.js
@@ -0,0 +1,31 @@
+var milliSeconds = document.getElementById('mlseconds');
+var seconds = document.getElementById('seconds');
+var minutes = document.getElementById('minute');
+var houers = document.getElementById('houers');
+
+var i = 0;
+function milliSec() {
+  if (i < 10) {
+    milliSeconds.innerHTML = '00' + i;
+  } else if (i < 100) {
+    milliSeconds.innerHTML = '0' + i;
+  } else if (i < 1000){
+    milliSeconds.innerHTML =  i;
+  } else {
+    i = 0;
+  }
+  i++;
+};
+
+var s = 0;
+function sec() {
+  if (s < 10) {
+    seconds.innerHTML = '0' + s;
+  }
+  s++;
+};
+
+
+
+setInterval(milliSec, 1);
+setInterval(sec, 1000);
