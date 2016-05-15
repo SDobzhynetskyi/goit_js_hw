d73704cfff8a075705d8f33ce29e8592da681ef3
Sergii Dobzhynetskyi <Sergii Dobzhynetskyi>
2016-05-15 22:21:41 +0300

    tab_option and hide_help

--- js_hw7-8/js/script2.js
+++ js_hw7-8/js/script2.js
@@ -1,15 +1,13 @@
 $(document).ready(function () {
   $(".help").hide();
 
-  $(".dataInput input").mouseover(function () {
-    var activeInput = $(this).attr("class");
-      $("." +activeInput).animate({opacity: "show" }, "slow");
-    }, function () {
-      $("." +activeInput).animate({opacity: "hide"}, "slow");
-    });
+  $(".dataInput input").hover(function () {
+    var activeHide = $(this).attr("class");
+    $(this).next("span").animate({opacity: "show"}, "slow");
+  }, function () {
+    $(this).next("span").animate({opacity: "hide"}, "slow");
+  })
 });
-
-
 
   $("#helpButton").click(function () {
     $(".help").animate({opacity: "show"}, "slow");
